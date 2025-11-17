b70c3b80a6782b7d60aff0e0d7dce90fee7fe6a2
Gustavo Henrique <157027244+GustavoHx09@users.noreply.github.com>
2025-11-17 13:59:37 -0300

    Feat: commit teste

--- includes/conn.php
+++ includes/conn.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "";
-$dbname = "";
-$username = "";
-$password = "";
+$servername = "seu servidor";
+$dbname = "seu banco";
+$username = "seu user";
+$password = "sua senha";
 
 global $conn;
 
 //faz conexao com o banco
 try {
 
-    $conn = new PDO("mysql:host=;dbname=", $username, $password);
+    $conn = new PDO("mysql:host=".$servername.";dbname=".$dbname."", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo "Falha na conexao: " . $e->getMessage();
